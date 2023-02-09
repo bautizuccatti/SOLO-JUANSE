3a71fb7a08b19b969e2465ff3f22ae5181c866b8
bautista zuccatti <zuccattibautista@gmail.com>
2023-02-08 22:19:29 -0300

    le agrego animaciones con js a el header y a los discos en discografia

---
+++ js/discografia.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal();
+
+    sr.reveal(' .disco', {
+        duration: 3000,
+        origin: 'buttom',
+        distance: '-100px'
+    });
+
+    window.sr = ScrollReveal();
+
+    sr.reveal(' .header-container', {
+        duration: 2000,
+        origin: 'buttom',
+        distance: '-100px'
+    });
+
